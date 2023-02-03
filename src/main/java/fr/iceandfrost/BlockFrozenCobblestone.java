2ae5810c733da5b21376ea371cf791b9b4297817
diamons <romain.hure@epitech.eu>
2023-02-03 11:26:49 +0100

    FIX(<Main.java>): renamed folder path.

---
+++ src/main/java/fr/iceandfrost/BlockFrozenCobblestone.java
@@ -0,0 +1,12 @@
+package fr.iceandfrost;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockFrozenCobblestone extends Block
+{
+    protected BlockFrozenCobblestone(Material material)
+    {
+        super(material);
+    }
+}
---
+++ src/main/java/fr/iceandfrost/item/ItemFrostPickaxe.java
@@ -0,0 +1,11 @@
+package fr.iceandfrost.item;
+
+import net.minecraft.item.ItemPickaxe;
+
+public class ItemFrostPickaxe extends ItemPickaxe
+{
+    public ItemFrostPickaxe(ToolMaterial material)
+    {
+        super(material);
+    }
+}
